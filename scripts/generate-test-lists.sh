7a74c8d8bda32c55653d502d31b3bfb0bdc95b04
Flynn Hembroff <flynnhembroff@gmail.com>
2022-06-28 23:32:20 -0400

    Testing possible change

--- scripts/generate-test-lists.sh
+++ scripts/generate-test-lists.sh
@@ -37,16 +37,12 @@ function make_file_names_fully_qualified_class_paths {
 # The temporary file used in the following function
 TEMP=temp_for_categorising_tests.txt
 
-# This functions consumes $REMAINING_TEST_FILE and determines which of those files have the listed Junit category (CATEGORY) in them
-# such as BitBucketTest.class, it then removes those test from $REMAINING_TEST_FILE and adds them to $OUTPUT_TEST_FILE
+# This functions consumes $IT_LIST and determines which of those files have the listed Junit category (CATEGORY) in them
+# such as BitBucketTest.class
 # Note: Does not acknowledge comments
 function generate_test_list {
   # Reset or create temp file
-  : > "$PREFIX"/"$TEMP"
-  grep -l "$CATEGORY" $(cat "$PREFIX"/"$REMAINING_TEST_FILE") > "$PREFIX"/"$OUTPUT_TEST_FILE"
-  grep -v -x -f "$PREFIX"/"$OUTPUT_TEST_FILE" "$PREFIX"/"$REMAINING_TEST_FILE" > "$PREFIX"/"$TEMP"
-  cp "$PREFIX"/"$TEMP" "$PREFIX"/"$REMAINING_TEST_FILE"
-
+  grep -l "$CATEGORY" $(cat "$PREFIX"/"$IT_LIST") > "$PREFIX"/"$OUTPUT_TEST_FILE"
 }
 
 
@@ -61,50 +57,45 @@ PREFIX="$BASE_PREFIX""/IT"
 mkdir -p "$PREFIX"
 
 
-REMAINING_TEST_FILE=non-confidential.txt
+IT_LIST=all.txt
 
 # Using same wild card patterns the Failsafe Plugin uses
+# Get all
 # https://maven.apache.org/surefire/maven-failsafe-plugin/examples/inclusion-exclusion.html
-find . -name "*IT\.java" -or -name "IT*\.java" -or -name "*ITCase\.java" > "$PREFIX"/"$REMAINING_TEST_FILE"
-
-
-# Get Toil ITs
-CATEGORY="ToilCompatibleTest.class\|ToilOnlyTest.class"
-OUTPUT_TEST_FILE=toil.txt
-generate_test_list
-FILE_TO_CHANGE="$PREFIX"/"$OUTPUT_TEST_FILE"
+find . -name "*IT\.java" -or -name "IT*\.java" -or -name "*ITCase\.java" > "$PREFIX"/all.txt
+FILE_TO_CHANGE="$PREFIX"/all.txt
 make_file_names_fully_qualified_class_paths
 
 
-# Get Singularity Tests
-CATEGORY=SingularityTest.class
-OUTPUT_TEST_FILE=singularity.txt
-generate_test_list
-FILE_TO_CHANGE="$PREFIX"/"$OUTPUT_TEST_FILE"
-make_file_names_fully_qualified_class_paths
+## Get Toil ITs
+#CATEGORY="ToilCompatibleTest.class\|ToilOnlyTest.class"
+#OUTPUT_TEST_FILE=toil.txt
+#generate_test_list
+#FILE_TO_CHANGE="$PREFIX"/"$OUTPUT_TEST_FILE"
+#make_file_names_fully_qualified_class_paths
+#
+## Get confidential tests
+#CATEGORY="ConfidentialTest.class\|BaseIT"
+#OUTPUT_TEST_FILE=confidential.txt
+#generate_test_list
+#
+#IT_LIST=confidential.txt
+#
+## Get confidential tool tests
+#CATEGORY="ToolTest.class"
+#OUTPUT_TEST_FILE=tool_confidential.txt
+#generate_test_list
+#
+#
+#
+## Get confidential all tests that are not tool
+#OUTPUT_TEST_FILE=workflow_confidential.txt
+#grep -v -x -f "$PREFIX"/tool_confidential.txt "$PREFIX"/confidential.txt > "$PREFIX"/"$OUTPUT_TEST_FILE"
+#
+#
+## Get non-confidential tests
+#grep -v -x -f "$PREFIX"/confidential.txt "$PREFIX"/all.txt > "$PREFIX"/non-confidential.txt
 
 
-# Get ALL Confidential Tests
-CATEGORY="ConfidentialTest.class\|BaseIT"
-# Many confidential tests extend BaseIT which has the category of ConfidentialTest
-OUTPUT_TEST_FILE=workflow_confidential.txt
-generate_test_list
-
-# Convert all non-confidential test file names to fully qualified class paths
-FILE_TO_CHANGE="$PREFIX"/"$REMAINING_TEST_FILE"
-make_file_names_fully_qualified_class_paths
-
-# Now set remaining test file to be confidential_workflow
-REMAINING_TEST_FILE=workflow_confidential.txt
-
-CATEGORY=ToolTest.class
-OUTPUT_TEST_FILE=tool_confidential.txt
-generate_test_list
-FILE_TO_CHANGE="$PREFIX"/"$OUTPUT_TEST_FILE"
-make_file_names_fully_qualified_class_paths
-
-# Convert all confidential workflow test file names to fully qualified class paths
-FILE_TO_CHANGE="$PREFIX"/"$REMAINING_TEST_FILE"
-make_file_names_fully_qualified_class_paths
 
 
